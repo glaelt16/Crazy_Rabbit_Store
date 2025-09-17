8cc74b4618950ffa8bb00fe7fe6e71bc7c033e90
glaelt16 <glaelt@yahoo.com>
2025-09-16 21:30:56 -0400

    Amazon link

--- src/app/ui/product-card/product-card.component.ts
+++ src/app/ui/product-card/product-card.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input, inject } from '@angular/core';
 import { Product } from '../../core/models/product.model';
 import { CartService } from '../../core/services/cart.service';
+import { CommonModule  } from '@angular/common';
 
 @Component({
   selector: 'app-product-card',
+  imports: [CommonModule],
   standalone: true,
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.scss']
