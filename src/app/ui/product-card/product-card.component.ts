cf6fa0baeb9d54958289b643d6841996b4912498
glaelt16 <glaelt@yahoo.com>
2025-09-18 13:59:43 -0400

    roll back

--- src/app/ui/product-card/product-card.component.ts
+++ src/app/ui/product-card/product-card.component.ts
@@ -2,15 +2,13 @@ import { Component, Input, inject } from '@angular/core';
 import { Product } from '../../core/models/product.model';
 import { CartService } from '../../core/services/cart.service';
 import { CommonModule } from '@angular/common';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-product-card',
-  imports: [CommonModule, MatCardModule, MatButtonModule],
   standalone: true,
+   imports: [CommonModule],
   templateUrl: './product-card.component.html',
-  styleUrls: ['./product-card.component.scss'],
+  styleUrls: ['./product-card.component.scss']
 })
 export class ProductCardComponent {
   @Input() product!: Product;
