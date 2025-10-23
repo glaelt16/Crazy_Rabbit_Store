18fd0a62b11777321eca48388e4c49767fbc0736
glaelt16 <glaelt@yahoo.com>
2025-10-23 14:16:32 -0400

    evr

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: '/api',
-  stripePublicKey: 'pk_live_placeholder'
+  apiUrl: 'https://crzyrabbit.com/api',
+  stripePublicKey: 'pk_live_51SAGlHD95CW6rARxWbjDDqV3uzMO5s3pFhqVTbuWLJflwLWGFe1ZNCFYI9d3Rg2kPlg0mhcHR5bV18s4DFqwLBvr00rHjVDENs'
 };
